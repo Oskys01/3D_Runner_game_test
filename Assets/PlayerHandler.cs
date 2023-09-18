5f3e6cd110bcdb0325543789de25cbe7552e665b
SMK\210-10 <oskaras.dulka@stud.smk.lt>
2023-09-18 12:09:35 +0300

    Added inputs and assets

---
+++ Assets/PlayerHandler.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerHandler : MonoBehaviour
+{
+    private float runSpeed = 1.0f;
+    private float sideSpeed = 5.0f;
+    public float jumpSpeed = 2.5f;
+    private Vector2 input = Vector2.zero;
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        input = context.ReadValue<Vector2>();
+    }
+
+    public void OnJump(){
+        GetComponent<Rigidbody>().AddForce(Vector3.up * jumpSpeed, ForceMode.Impulse);
+    }
+
+    
+    void Update()
+    {
+        transform.Translate(new Vector3(input.x * sideSpeed, 0, runSpeed) * Time.deltaTime);
+    }
+}
