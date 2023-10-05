522054c9336b469b74f5a480bd65bd2375330a90
SMK\210-10 <oskaras.dulka@stud.smk.lt>
2023-10-05 12:15:08 +0300

    Improved things on collision

--- Assets/PlayerHandler.cs
+++ Assets/PlayerHandler.cs
@@ -1,17 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerHandler : MonoBehaviour
 {
-    private float runSpeed = 5.0f;
+    private float runSpeed = 7.5f;
     private float sideSpeed = 5.0f;
-    public float jumpSpeed = 5f;
+    public float jumpSpeed = 5.5f;
     private Vector2 input = Vector2.zero;
 
     private bool canMove = true;
+    private bool canJump = false;
+    
 
     public void OnMove(InputAction.CallbackContext context)
     {
@@ -29,18 +32,33 @@ public class PlayerHandler : MonoBehaviour
     void Update()
     {
         transform.Translate(new Vector3(input.x * sideSpeed, 0, canMove ? runSpeed : 0) * Time.deltaTime);
+        
     }
 
-    public async Task OnCollisionEnter(Collision collision)
+    public async void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.name.StartsWith("Obs"))
         {
+            sideSpeed = 0f;
+            
+
             canMove = false;
+            
             GameObject.Find("Remy").GetComponent<Animator>().Play("FallingDown");
-            GetComponent<Rigidbody>().AddForce(Vector3.back, ForceMode.Impulse);
-            await Task.Delay(3000);
+            GetComponent<Rigidbody>().AddForce(Vector3.back * 4, ForceMode.Impulse);
+            await Task.Delay(1900);
+            sideSpeed = 5.5f;
+
+            await Task.Delay(200);
             canMove = true;
+            
         }
+
+        if (collision.gameObject.name.StartsWith("Plane"))
+        {
+            canJump = true;
+        }
+            
         
     }
 }
